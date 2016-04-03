843898ec0f7376f90a3384c7eae7905ab35a36f6
ferhung2013 <ferhung@163.com>
2016-04-03 21:35:20 +0800

    init: add detection of charging mode(2/2).
    Based on Prashant Somashekar's patch which is in turn based on techomancer's
    patch http://goo.gl/I19GG.
    
    When BOARD_CHARGING_MODE_BOOTING_LPM is set, init will read that
    value (usually from /sys) and enable charging mode when set to 1.
    
    This differs from the original patch in that Samsung-specific lpm.rc
    is not loaded, but instead, init.rc is loaded using the "charger" class,
    which is how AOSP devices handle charging.
    
    The change is because the either the SELinux or the init.rc changes is
    causing lpm.rc to not work, and porting lpm.rc to the Android system
    is easier than trying to fix it.

--- init/init.cpp
+++ init/init.cpp
@@ -998,6 +998,25 @@ static void selinux_initialize(bool in_kernel_domain) {
     }
 }
 
+static int charging_mode_booting(void)
+{
+#ifndef BOARD_CHARGING_MODE_BOOTING_LPM
+    return 0;
+#else
+    int f;
+    char cmb;
+    f = open(BOARD_CHARGING_MODE_BOOTING_LPM, O_RDONLY);
+    if (f < 0)
+        return 0;
+
+    if (1 != read(f, (void *)&cmb,1))
+        return 0;
+
+    close(f);
+    return ('1' == cmb);
+#endif
+}
+
 int main(int argc, char** argv) {
     if (!strcmp(basename(argv[0]), "ueventd")) {
         return ueventd_main(argc, argv);
@@ -1109,7 +1128,7 @@ int main(int argc, char** argv) {
 
     // Don't mount filesystems or start core system services in charger mode.
     char bootmode[PROP_VALUE_MAX];
-    if (property_get("ro.bootmode", bootmode) > 0 && strcmp(bootmode, "charger") == 0) {
+    if ((property_get("ro.bootmode", bootmode) > 0 && strcmp(bootmode, "charger") == 0 )  || charging_mode_booting()){
         action_for_each_trigger("charger", action_add_queue_tail);
     } else {
         action_for_each_trigger("late-init", action_add_queue_tail);
