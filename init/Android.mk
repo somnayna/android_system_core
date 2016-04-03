d8ce40ba965cff4c51377779c8da82b279d0ef82
ferhung2013 <ferhung@163.com>
2016-04-03 21:25:43 +0800

    init: add detection of charging mode(1/2).
    Based on Prashant Somashekar's patch which is in turn based on techomancer's
    patch http://goo.gl/I19GG.
    
    When BOARD_CHARGING_MODE_BOOTING_LPM is set, init will read that
    value (usually from /sys) and enable charging mode when set to 1.
    
    This differs from the original patch in that Samsung-specific lpm.rc
    is not loaded, but instead, init.rc is loaded using the "charger" class,
    which is how AOSP devices handle charging.
    
    The change is because the either the SELinux or the init.rc changes is
    causing lpm.rc to not work, and porting lpm.rc to the Android system
    is easier than trying to fix it.

--- init/Android.mk
+++ init/Android.mk
@@ -58,6 +58,14 @@ ifeq ($(TARGET_BOARD_PLATFORM),mt6592)
 LOCAL_CFLAGS += -DMTK_MT6592
 endif
 
+SYSTEM_CORE_INIT_DEFINES := BOARD_CHARGING_MODE_BOOTING_LPM
+
+$(foreach system_core_init_define,$(SYSTEM_CORE_INIT_DEFINES), \
+  $(if $($(system_core_init_define)), \
+    $(eval LOCAL_CFLAGS += -D$(system_core_init_define)=\"$($(system_core_init_define))\") \
+  ) \
+)
+
 LOCAL_MODULE:= init
 LOCAL_C_INCLUDES += \
     system/extras/ext4_utils \
